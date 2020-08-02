cedd6dac2a6bc253ec8a2cecb248bcd1256e731c
Swann <swann@devloup.dev>
2020-08-02 22:12:46 +0200

    first commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00719b5d46231d36d471e501518877d9'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6652c16a-2e6b-409a-8065-b5cdd4012040'
+};
